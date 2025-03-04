82f53d5fb5b44019144a73280cf4df870efe48b4
Lavina-23 <lavinakohai3227@gmail.com>
2025-03-04 14:51:53 +0700

    (js3) praktikum 5

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\KategoriController;
 use App\Http\Controllers\LevelController;
 use App\Http\Controllers\ProductsController;
 use App\Http\Controllers\TransactionController;
@@ -9,6 +10,7 @@ use App\Http\Controllers\TransactionController;
 Route::get('/', [HomeController::class, 'index']);
 
 Route::get('/level', [LevelController::class, 'index']);
+Route::get('/kategori', [KategoriController::class, 'index']);
 
 Route::prefix('category')->group(function () {
     Route::get('food-baverage', [ProductsController::class, 'FoodBaverage']);
