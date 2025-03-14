8bfce42ef3ec786694f3bf1e6c2af644e92cc5bf
Lavina-23 <lavinakohai3227@gmail.com>
2025-03-14 11:11:18 +0700

    feat(js4): praktikum 2.3

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -7,7 +7,7 @@
 
 <body>
     <h1>Data User</h1>
-    <table border="1" cellpadding="2" cellspacing="0">
+    {{-- <table border="1" cellpadding="2" cellspacing="0">
         <tr>
             <td>ID</td>
             <td>Username</td>
@@ -20,6 +20,14 @@
             <td>{{ $data->username }}</td>
             <td>{{ $data->nama }}</td>
             <td>{{ $data->level_id }}</td>
+        </tr>
+    </table> --}}
+    <table border="1" cellpadding="2" cellspacing="0">
+        <tr>
+            <th>Jumlah Pengguna</th>
+        </tr>
+        <tr>
+            <td>{{ $data }}</td>
         </tr>
     </table>
 </body>
